7ccc9a5539be52035e51fdea96180678ee675112
sai135829 <sait18653@gmail.com>
2025-12-16 14:17:54 +0530

    Add files via upload

---
+++ factorial2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	long int i,n,fact=1;
+	printf("enter any number");
+	scanf("%ld",&n);
+	
+	for(i=1;i<=n;i++)
+	{
+	fact=fact*i;
+    }
+		
+		printf("factorial in=%ld",fact);
+		return 0;
+		
+	}
+	
