f3988f92cdcc459bb095b28be3e998106f93e59a
sai135829 <sait18653@gmail.com>
2025-12-16 14:11:34 +0530

    Add files via upload

---
+++ even or odd numbers.cpp
@@ -0,0 +1,17 @@
+//even or odd numbers.
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the value");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("even");
+	}
+	else
+	{
+		printf("odd");
+	}
+	return 0;
+}
