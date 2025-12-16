295797cf2cf5cad4b374487cbef197fdd259f2d3
sai135829 <sait18653@gmail.com>
2025-12-16 15:19:42 +0530

    Add files via upload

---
+++ sumN.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int i,n,sum=0;
+	printf("enter any number");
+	scanf("%d",&n);
+	
+		for(i=1;i<=n;i++)
+	{
+		sum=sum+i;
+	}
+	printf("motham kalipithe  in=%d",sum);
+	return 0;
+    }
