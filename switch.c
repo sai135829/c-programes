a22afa469ab4197b97254846f038f9308027b5fc
sai135829 <sait18653@gmail.com>
2025-12-16 15:20:15 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int x;
+	printf("enter x value");
+	scanf("%d",&x);
+	switch(x>0)
+	{
+	 case 1:printf("it is positive");	
+		    break;
+	 case 2:printf("it is negative");	
+	        break;	
+	  }
+	   return 0;
+	
+	
+	
+	
+}
